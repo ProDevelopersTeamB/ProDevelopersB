248b084fc82ffec06803149e25482ff256dcebcb
Musasin <c0112051b9@edu.teu.ac.jp>
2019-12-14 15:52:13 +0900

    add result scene

---
+++ Assets/Scripts/ScoreResult.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreResult : MonoBehaviour
+{
+    // 仮
+    int kyori = 12345;
+    float time = 125.204f;
+    int score = 22334455;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager.Instance.PlayBGM("Result");
+        string text = "キョリ: " + kyori + "\n\n";
+        text += "タイム: " + time + " sec.\n\n";
+        text += "とくてん: " + score + "\n\n\n";
+        text += "ひょうか: " + "S" + "\n\n";
+        GetComponent<Text>().text = text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoToTitle()
+    {
+        SceneManager.LoadScene("GashaScene");
+    }
+}
