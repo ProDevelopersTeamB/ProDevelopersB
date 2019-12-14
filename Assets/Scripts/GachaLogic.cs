51d0aca906f772db4bd19f8cb7320f6804fa1929
Musasin <c0112051b9@edu.teu.ac.jp>
2019-12-14 13:51:09 +0900

    add character * 5

--- Assets/Scripts/GachaLogic.cs
+++ Assets/Scripts/GachaLogic.cs
@@ -11,8 +11,9 @@ public class GachaLogic : MonoBehaviour
     enum State
     {
         Idle = 0,
-        Lottery = 1,
-        Result = 2,
+        Roulette = 1,
+        Lottery = 2,
+        Result = 3,
     }
     State state;
 
@@ -30,31 +31,39 @@ public class GachaLogic : MonoBehaviour
         {
             int rarityRand = Random.Range(1, 100);
             int rarity;
+            float coefficient;
             if (rarityRand <= 5)
             {
                 rarity = 5;
+                coefficient = 1.5f;
             } else if (rarityRand <= 15)
             {
                 rarity = 4;
-
+                coefficient = 1.25f;
             }
             else if (rarityRand <= 30)
             {
                 rarity = 3;
+                coefficient = 1.0f;
             }
             else if (rarityRand <= 60)
             {
                 rarity = 2;
+                coefficient = 0.75f;
             }
             else
             {
                 rarity = 1;
+                coefficient = 0.5f;
             }
 
             int charaRand = Random.Range(0, characters.Count - 1);
             GameObject resultObject = characters[charaRand];
             Character resultCharacter = resultObject.GetComponent<Character>();
             resultCharacter.rarity = rarity;
+            resultCharacter.speed *= coefficient;
+            resultCharacter.jump *= coefficient;
+
             Instantiate(resultObject);
             string text = "レアリティ:";
             for (int i=0; i < resultCharacter.rarity; i++)
