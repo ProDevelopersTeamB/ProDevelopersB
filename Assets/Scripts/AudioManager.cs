83ca5a5ba3525d766832c9e21f5ac565dec892dd
Musasin <c0112051b9@edu.teu.ac.jp>
2019-12-14 15:03:00 +0900

    add title, any doubutsu

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,227 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class AudioManager : SingletonMonoBehaviour<AudioManager>
+{
+    public List<AudioClip> BGMList;
+    public List<AudioClip> SEList;
+    public List<AudioClip> exVoiceList;
+
+    // 同時に再生可能なSE数の上限
+    private readonly int SE_COUNT_LIMIT = 100;
+
+    private AudioSource bgmSource = null;
+    private List<AudioSource> seSources = null;
+    private AudioSource exVoiceSource = null;
+    private Dictionary<string, AudioClip> bgmDict = null;
+    private Dictionary<string, AudioClip> seDict = null;
+    private Dictionary<string, AudioClip> exVoiceDict = null;
+
+    private bool isFadeOut = false;
+    private bool isFadeIn = false;
+    private float fadeToVolume = 0.0f;
+
+    public void Awake()
+    {
+        // Audio Listener がなければ作る
+        if (FindObjectsOfType(typeof(AudioListener)).All(o => !((AudioListener)o).enabled))
+        {
+            this.gameObject.AddComponent<AudioListener>(); //DEBUG
+        }
+        // Audio Source を作る
+        this.bgmSource = this.gameObject.AddComponent<AudioSource>();
+        this.seSources = new List<AudioSource>();
+        this.exVoiceSource = this.gameObject.AddComponent<AudioSource>();
+
+        // BGM/SEリストの初期化
+        this.bgmDict = new Dictionary<string, AudioClip>();
+        this.seDict = new Dictionary<string, AudioClip>();
+        this.exVoiceDict = new Dictionary<string, AudioClip>();
+
+        // Inspectorから設定したAudioClipをリストに格納する
+        Action<Dictionary<string, AudioClip>, AudioClip> addClipDict = (dict, c) =>
+        {
+            if (!dict.ContainsKey(c.name))
+            {
+                dict.Add(c.name, c);
+            }
+        };
+
+        AudioClip[] seFiles = Resources.LoadAll<AudioClip>("Sounds/SE");
+        foreach (AudioClip var in seFiles)
+        {
+            this.SEList.Add(var);
+        }
+
+        AudioClip[] bgmFiles = Resources.LoadAll<AudioClip>("Sounds/BGM");
+        foreach (AudioClip var in bgmFiles)
+        {
+            this.BGMList.Add(var);
+        }
+
+        AudioClip[] exVoiceFiles = Resources.LoadAll<AudioClip>("Sounds/ExVoice");
+        foreach (AudioClip var in exVoiceFiles)
+        {
+            this.exVoiceList.Add(var);
+        }
+
+        this.BGMList.ForEach(bgm => addClipDict(this.bgmDict, bgm));
+        this.SEList.ForEach(se => addClipDict(this.seDict, se));
+        this.exVoiceList.ForEach(exVoice => addClipDict(this.exVoiceDict, exVoice));
+    }
+
+    public void Update()
+    {
+        if (isFadeOut)
+        {
+            this.bgmSource.volume = this.bgmSource.volume * 0.98f;
+            if (this.bgmSource.volume < 0.01f)
+            {
+                this.StopBGM();
+            }
+        }
+        if (isFadeIn)
+        {
+            if (fadeToVolume >= this.bgmSource.volume)
+            {
+                this.bgmSource.volume += 0.002f;
+            }
+            else
+            {
+                isFadeIn = false;
+            }
+        }
+    }
+
+    public void PlaySE(string seName, float volume = 1.0f, bool loop = false)
+    {
+        if (!this.seDict.ContainsKey(seName))
+            throw new ArgumentException(seName + " not found", "seName");
+
+        AudioSource source = this.seSources.FirstOrDefault(s => !s.isPlaying);
+        if (source == null)
+        {
+            if (this.seSources.Count >= this.SE_COUNT_LIMIT)
+            {
+                Debug.Log("SE AudioSource is full");
+                return;
+            }
+
+            source = this.gameObject.AddComponent<AudioSource>();
+            this.seSources.Add(source);
+        }
+
+        source.clip = this.seDict[seName];
+        source.volume = volume;
+        source.pitch = 1.0f;
+        source.loop = loop;
+        source.Play();
+    }
+
+    public void StopSE()
+    {
+        this.seSources.ForEach(s => s.Stop());
+    }
+
+    public void StopSE(string seName)
+    {
+        foreach (var source in this.seSources)
+        {
+            if (source.clip.name == seName)
+            {
+                source.Stop();
+            }
+        }
+    }
+
+    public void PlaySE(string seName)
+    {
+        PlaySE(seName, 0.3f, false);
+    }
+
+    public void PlayExVoice(string exVoiceName, float volume = 1.0f, bool loop = false)
+    {
+        if (!this.exVoiceDict.ContainsKey(exVoiceName))
+            throw new ArgumentException(exVoiceName + " not found", "exVoiceName");
+        if (this.exVoiceSource.clip == this.exVoiceDict[exVoiceName])
+            return;
+        this.exVoiceSource.Stop();
+        this.exVoiceSource.clip = this.exVoiceDict[exVoiceName];
+        this.exVoiceSource.loop = loop;
+        this.exVoiceSource.volume = volume;
+        this.exVoiceSource.pitch = 1;
+        this.exVoiceSource.Play();
+        isFadeOut = false;
+    }
+
+    public bool IsPlayingExVoice()
+    {
+        return this.exVoiceSource.isPlaying;
+    }
+
+    public void PlayBGM(string bgmName, float volume = 1.0f, bool loop = true)
+    {
+        if (!this.bgmDict.ContainsKey(bgmName))
+            throw new ArgumentException(bgmName + " not found", "bgmName");
+        if (this.bgmSource.clip == this.bgmDict[bgmName])
+            return;
+        this.bgmSource.Stop();
+        this.bgmSource.clip = this.bgmDict[bgmName];
+        this.bgmSource.loop = loop;
+        this.bgmSource.volume = volume;
+        this.bgmSource.pitch = 1;
+        this.bgmSource.Play();
+        isFadeOut = false;
+    }
+
+    public void FadeInBGM(string bgmName, float volume = 1.0f, bool loop = true)
+    {
+        PlayBGM(bgmName, 0.0f, loop);
+        fadeToVolume = volume;
+        isFadeIn = true;
+    }
+
+    public void StopBGM()
+    {
+        this.bgmSource.Stop();
+        this.bgmSource.clip = null;
+        isFadeOut = false;
+    }
+
+    public void FadeOutBGM()
+    {
+        isFadeOut = true;
+    }
+
+    public void SetBGMPitch(float pitch)
+    {
+        this.bgmSource.pitch = pitch;
+    }
+
+    public void SetSEPitch(string seName, float pitch)
+    {
+        foreach (var source in this.seSources)
+        {
+            if (source.clip.name == seName)
+            {
+                source.pitch = pitch;
+            }
+        }
+    }
+
+    public bool IsPlayingSE()
+    {
+        foreach (var source in this.seSources)
+        {
+            if (source.isPlaying)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- Assets/Scripts/GachaLogic.cs
+++ Assets/Scripts/GachaLogic.cs
@@ -7,13 +7,16 @@ public class GachaLogic : MonoBehaviour
 {
     public List<GameObject> characters;
     Text resultText;
+    float drumRollPosX;
+    float time;
 
     enum State
     {
         Idle = 0,
-        Roulette = 1,
-        Lottery = 2,
-        Result = 3,
+        DrumRollInit,
+        DrumRoll,
+        Lottery,
+        Result,
     }
     State state;
 
@@ -21,64 +24,101 @@ public class GachaLogic : MonoBehaviour
     void Start()
     {
         state = State.Idle;
-        resultText = GameObject.Find("ResultText").GetComponent<Text>();
+        drumRollPosX = -6.0f;
+        time = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (state == State.Lottery)
+        time += Time.deltaTime;
+        switch (state)
         {
-            int rarityRand = Random.Range(1, 100);
-            int rarity;
-            float coefficient;
-            if (rarityRand <= 5)
-            {
-                rarity = 5;
-                coefficient = 1.5f;
-            } else if (rarityRand <= 15)
-            {
-                rarity = 4;
-                coefficient = 1.25f;
-            }
-            else if (rarityRand <= 30)
-            {
-                rarity = 3;
-                coefficient = 1.0f;
-            }
-            else if (rarityRand <= 60)
-            {
-                rarity = 2;
-                coefficient = 0.75f;
-            }
-            else
-            {
-                rarity = 1;
-                coefficient = 0.5f;
-            }
+            case State.DrumRollInit:
+                resultText = GameObject.Find("ResultText").GetComponent<Text>();
+                for (int i=0; i<5; i++)
+                {
+                    foreach (GameObject charaObj in characters)
+                    {
+                        GameObject obj = Instantiate(charaObj);
+                        obj.transform.parent = this.transform;
+                        obj.transform.position = new Vector2(drumRollPosX, 0);
+                        obj.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0);
+                        drumRollPosX += 2.0f;
+                    }
+                }
+                state = State.DrumRoll;
+                break;
+            case State.DrumRoll:
+                this.transform.position = new Vector2(this.transform.position.x - 0.2f, 0);
+                if (!AudioManager.Instance.IsPlayingSE())
+                {
+                    AudioManager.Instance.PlaySE("RollFinish");
+                    state = State.Lottery;
+                    foreach (Transform n in this.transform)
+                    {
+                        GameObject.Destroy(n.gameObject);
+                    }
+                }
 
-            int charaRand = Random.Range(0, characters.Count - 1);
-            GameObject resultObject = characters[charaRand];
-            Character resultCharacter = resultObject.GetComponent<Character>();
-            resultCharacter.rarity = rarity;
-            resultCharacter.speed *= coefficient;
-            resultCharacter.jump *= coefficient;
 
-            Instantiate(resultObject);
-            string text = "レアリティ:";
-            for (int i=0; i < resultCharacter.rarity; i++)
-            {
-                text += "★";
-            }
-            text += "\nスピード: " + resultCharacter.speed;
-            text += "\nジャンプ: " + resultCharacter.jump;
-            resultText.text = text;
-            state = State.Result;
+                break;
+            case State.Lottery:
+                int rarityRand = Random.Range(1, 100);
+                int rarity;
+                float coefficient;
+                if (rarityRand <= 5)
+                {
+                    rarity = 5;
+                    coefficient = 1.5f;
+                }
+                else if (rarityRand <= 15)
+                {
+                    rarity = 4;
+                    coefficient = 1.25f;
+                }
+                else if (rarityRand <= 30)
+                {
+                    rarity = 3;
+                    coefficient = 1.0f;
+                }
+                else if (rarityRand <= 60)
+                {
+                    rarity = 2;
+                    coefficient = 0.75f;
+                }
+                else
+                {
+                    rarity = 1;
+                    coefficient = 0.5f;
+                }
+
+                int charaRand = Random.Range(0, characters.Count - 1);
+                GameObject resultObject = characters[charaRand];
+                Character resultCharacter = resultObject.GetComponent<Character>();
+                resultCharacter.rarity = rarity;
+                resultCharacter.speed *= coefficient;
+                resultCharacter.jump *= coefficient;
+
+                Instantiate(resultObject);
+                string text = "レアリティ:";
+                for (int i = 0; i < resultCharacter.rarity; i++)
+                {
+                    text += "★";
+                }
+                text += "\nスピード: " + resultCharacter.speed;
+                text += "\nジャンプ: " + resultCharacter.jump;
+                resultText.text = text;
+                state = State.Result;
+                break;
+            default:
+                break;
         }
     }
 
     public void StartGacha()
     {
-        state = State.Lottery;
+        AudioManager.Instance.PlaySE("DrumRoll");
+        state = State.DrumRollInit;
     }
 }
