5b076c50771102fea0e965840df21239e43c83c7
Musasin <c0112051b9@edu.teu.ac.jp>
2019-12-14 17:36:20 +0900

    add score sync

--- Assets/Scripts/PlayerCollider.cs
+++ Assets/Scripts/PlayerCollider.cs
@@ -12,6 +12,19 @@ public class PlayerCollider : MonoBehaviour
 
     private void gameOver()
     {
-        SceneManager.LoadScene("ResultScore");
+        StartCoroutine(this.invokeActionOnloadScene("ResultScore", () => {
+            var scoreResult = FindObjectOfType<ScoreResult>() as ScoreResult;
+            scoreResult.kyori = 10;
+            scoreResult.time = 20;
+            scoreResult.score = 30;
+        }));
+
+    }
+    private IEnumerator invokeActionOnloadScene(string sceneName, System.Action onLoad)
+    {
+        var asyncOp = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        yield return asyncOp;
+        onLoad.Invoke();
+        SceneManager.UnloadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
